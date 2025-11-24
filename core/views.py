35587fdd3757b6b169c3bf1287b27ad44166c37c
Edgar-Poot <edgar.dev25@gmail.com>
2025-11-24 14:55:45 -0600

    feat(views): agregar vistas y manejar template tags

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'title': "BuildTemplate 2025"})
+
+
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
