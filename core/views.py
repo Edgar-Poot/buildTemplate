fc4b0d15e184b642af62f826fc60a84745e12120
Edgar-Poot <edgar.dev25@gmail.com>
2025-11-24 15:09:31 -0600

    refactor(views/home): agregar template variable

--- core/views.py
+++ core/views.py
@@ -6,7 +6,7 @@ from django.views.generic.base import TemplateView
 class HomePageView(TemplateView):
     template_name = "core/home.html"
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {'title': "BuildTemplate 2025"})
+        return render(request, self.template_name, {'title': "BackendTemplate 2025"})
 
 
 class SamplePageView(TemplateView):
